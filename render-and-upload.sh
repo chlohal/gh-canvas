c251b53f0064d2f68b5d4d6c239a375e8edc5f05
chlohal <me@clh.sh>
2023-10-12 23:24:07 -0400

    Use latest precompiled

--- render-and-upload.sh
+++ render-and-upload.sh
@@ -57,7 +57,7 @@ function get_canvas_info() {
 HTML_RENDERER_FILENAME="$(mktemp)"
 
 function download_html_renderer() {
-    curl -o "$HTML_RENDERER_FILENAME" -L "https://github.com/chlohal/gh-canvas/releases/download/v0.07/render-html"
+    curl -o "$HTML_RENDERER_FILENAME" -L "https://github.com/chlohal/gh-canvas/releases/download/v0.08/render-html"
     chmod +x "$HTML_RENDERER_FILENAME"
 }
 
